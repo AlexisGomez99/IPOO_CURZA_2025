8cdff234b603a87a3414cf753cb2f2a297c5778d
AlexisGomez99 <ralexisge@gmail.com>
2025-10-20 10:24:36 -0300

    Comienzo tp 3

--- ricardo_alexis_gomez_espinoza_pt2_IPOO_CURZA_2025/main.py
+++
@@ -1,8 +0,0 @@
-from ManejadorDePersonaje import ManejadorDePersonaje
-
-
-
-manejador_de_personaje = ManejadorDePersonaje()
-manejador_de_personaje.crear_personajes()
-manejador_de_personaje.configurar_arsenal()
-print(manejador_de_personaje.listar_personajes())
